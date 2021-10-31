996ed06b635dc1d08471d3a6b384c6c20356eda1
Seid Muhie Yimam <seidymam@gmail.com>
2021-10-31 02:16:02 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,17 +2,17 @@ from distutils.core import setup
 setup(
   name = 'amseg',         # Amharic Sentence segmentation, tokenization and normalization tools
   packages = ['amseg'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  version = '0.3',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'This is an Amharic document segmentation and normalization tool',   # Give a short description about your library
   author = 'Seid Muhie Yimam',                   # Type in your name
   author_email = 'seid.muhie.yimam@uni-hamburg.de',      # Type in your E-Mail
   url = 'https://seyyaw.github.io/',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/uhh-lt/amharicprocessor/archive/refs/tags/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/uhh-lt/amharicprocessor/archive/refs/tags/v_03.tar.gz',    # I explain this later on
   keywords = ['Amharic', 'Amharic sentence splitter', 'Amharic document normalizer'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
-          'validators',
-          'beautifulsoup4',
+         # 'validators',
+        #  'beautifulsoup4',
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
